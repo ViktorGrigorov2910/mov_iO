e08ac3435e5d151125f4d569f91a46dafd52ee8e
viktor.grigorov <viktor_grigorov@abv.bg>
2022-06-20 18:02:38 +0300

    First exercise..

---
+++ app/src/main/java/com/io/movio/HomeScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.io.movio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
