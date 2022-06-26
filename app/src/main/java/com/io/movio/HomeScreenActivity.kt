a253de5cd7807f89b3f06813997c783a6d056a11
Viktor <viktor_grigorov@abv.bg>
2022-06-26 17:34:26 +0300

    Committing layout files for recycler view + adaptor

--- app/src/main/java/com/io/movio/HomeScreenActivity.kt
+++ app/src/main/java/com/io/movio/HomeScreenActivity.kt
@@ -1,35 +1,21 @@
 package com.io.movio
 
-
-
 import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
-import com.io.movio.model.MovieDTO
-
+import com.io.movio.fragments.MainFragment
+import com.io.movio.models.MovieDTO
 
 
 class HomeScreenActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
-        //Initialization of movies
-        val myListData: Array<MovieDTO> = arrayOf(
-            MovieDTO(1, "Movie1", "asdasdasd"),
-            MovieDTO(2, "Movie2", "asdasdasd"),
-            MovieDTO(3, "Movie3", "asdasdasd"),
-            MovieDTO(4, "Movie4", "asdasdasd"),
-            MovieDTO(5, "Movie5", "asdasdasd"),
-            MovieDTO(6, "Movie6", "asdasdasd"),
-            MovieDTO(7, "Movie7", "asdasdasd"),
-            MovieDTO(8, "Movie8", "asdasdasd")
-        )
 
-
-
-
+        val fragment = MainFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
 
 
     }
